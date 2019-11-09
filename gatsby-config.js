95ba9b056a1c65cff9a9331ec3a3c267da676fde
Andrew (Paradi) Alexander <gh@adrw.xyz>
2019-11-09 10:43:00 -0500

    Migrate to recipe theme (#165)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,46 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
 module.exports = {
   siteMetadata: {
-    title: "cook.adrw.xyz"
+    title: "Alexander Receipes",
+    shortTitle: "Recipes",
+    description: "Cook More, Together",
+    url: "https://cook.adrw.xyz", // No trailing slash allowed!
+    image: "/images/og-image.jpg", // Path to your image you placed in the 'static' folder
+    author: "The Alexanders",
+    intro: "Classic recipes you’ll want to keep making.",
+    menuLinks: [
+      {
+        name: "About",
+        slug: "/about/"
+      }
+    ],
+    footerLinks: [
+      {
+        name: "(C) 2009-2019 ADRW Digital",
+        href: "https://adrw.xyz"
+      },
+      {
+        name: "Andrew Paradi Alexander",
+        href: "https://andrew.fm"
+      },
+      {
+        name: "Built on Westegg",
+        href: "https://westegg.xyz"
+      }
+    ]
   },
   plugins: [
     {
-      resolve: "@westegg/gatsby-theme-digital-garden",
+      resolve: "@marinda/gatsby-theme-recipes",
       options: {
-        header: {
-          home: {
-            href: "/",
-            label: "/img/logos/ch.png"
-          },
-          links: [
-            {
-              href: "/recipes",
-              label: "Recipes"
-            },
-            {
-              href: "/notes",
-              label: "Notes"
-            }
-          ]
-        },
-        notesPath: "/notes",
-        projectsPath: "/recipes",
-        projects: "recipes"
+        title: "Alexander Receipes",
+        shortTitle: "Recipes"
       }
     }
   ]
-}
+};
